a139aa4282c69fe7a137118de009b15bc9e6ebe8
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-02-26 14:41:23 +0000

    feat:GoogleFonts サブセット化

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
     <html lang="ja">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Noto+Sans:wght@400;500;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Noto+Sans:wght@400;500;700&display=swap&text=%20-_:()%E6%9C%80%E7%B5%82%E6%9B%B4%E6%96%B0%E5%B9%B4%E6%9C%88%E6%97%A5%E3%81%94%E3%82%8DABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789."
           rel="stylesheet"
         />
       </head>
