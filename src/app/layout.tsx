39255c9d5bc7e230f9a5694ca9a93f54e560294a
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-02-26 19:12:57 +0900

    fix: NotoSans was not loaded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <head></head>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&family=Noto+Sans:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <Body>
         <Header>
           <HeaderLogo>CP Review (a01sa01to)</HeaderLogo>
